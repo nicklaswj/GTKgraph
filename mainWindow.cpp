312f95fdcfdd98fceb954031ad51af1e8be4e794
Jacobsen <nicklaswj@gmail.com>
2012-02-17 23:10:11 +0100

    modified:   mainWindow.cpp 	modified:   mainWindow.h Basic design made

--- mainWindow.cpp
+++ mainWindow.cpp
@@ -6,6 +6,7 @@ mainWindow::mainWindow()
 	mainBox = new Gtk::Box(Gtk::ORIENTATION_VERTICAL,0);
 	upperBox = new Gtk::Box();
 	lowerBox = new Gtk::Box();
+	toolbarBox = new Gtk::Box();
 	eqPaned = new Gtk::HPaned();
 	
 	test1 = new Gtk::Button();
@@ -18,11 +19,54 @@ mainWindow::mainWindow()
 	commandLine = new Gtk::Entry();
 	
 	//initial the window
-	set_border_width(10);
+	set_border_width(0);
 	resize(300,400);
 	
+	//Init toolbar and menu
+	refActionGroup = Gtk::ActionGroup::create();
+	
+		//filemenu
+		refActionGroup->add(Gtk::Action::create("FileMenu", "File"));
+		refActionGroup->add(Gtk::Action::create("FileOpen", Gtk::Stock::OPEN),
+			sigc::mem_fun(*this, &mainWindow::openSaved)
+		);
+		
+		refActionGroup->add(Gtk::Action::create("FileQuit", Gtk::Stock::QUIT),
+				    sigc::mem_fun(*this, &mainWindow::quitWindow)
+		);
+		
+	//Init UIManager
+	refUIManager = Gtk::UIManager::create();
+	refUIManager->insert_action_group(refActionGroup);
+	
+	Glib::ustring ui_info = 
+		"<ui>"
+		"	<menubar name='MenuBar'>"
+		"		<menu action='FileMenu'>"
+		"			<menuitem action='FileOpen'/>"
+		"			<separator/>"
+		"			<menuitem action='FileQuit'/>"
+		"		</menu>"
+		"	</menubar>"
+		"</ui>";
+		
+	try
+	  {
+	    refUIManager->add_ui_from_string(ui_info);
+	  }
+	  catch(const Glib::Error& ex)
+	  {
+	    std::cerr << "building menus failed: " <<  ex.what();
+	  }
+	  
+	
+	Gtk::Widget *MenuBar = refUIManager->get_widget("/MenuBar");
+	
+	if(MenuBar)
+		toolbarBox->pack_start(*MenuBar, Gtk::PACK_SHRINK);
 	
 	//Pack the upper and lower box
+	mainBox->pack_start(*toolbarBox, false, false, 0);
 	mainBox->pack_start(*upperBox);
 	mainBox->pack_start(*lowerBox, false, false, 0);
 	
@@ -37,5 +81,14 @@ mainWindow::mainWindow()
 }
 
 mainWindow::~mainWindow()
+{
+}
+
+void mainWindow::quitWindow()
+{
+	hide();
+}
+
+void mainWindow::openSaved()
 {
 }
--- mainWindow.h
+++ mainWindow.h
@@ -1,4 +1,5 @@
 #include <gtkmm.h>
+#include <iostream>
 
 class mainWindow : public Gtk::Window
 {
@@ -9,10 +10,18 @@ protected:
 	Gtk::Box *mainBox;
 	Gtk::Box *upperBox;
 	Gtk::Box *lowerBox;
+	Gtk::Box *toolbarBox;
 
 	Gtk::Entry *commandLine;
 	
 	Gtk::HPaned *eqPaned;
 	Gtk::Button *test1;
 	Gtk::Button *test2;
+	
+	Glib::RefPtr<Gtk::UIManager> refUIManager;
+	Glib::RefPtr<Gtk::ActionGroup> refActionGroup;
+	Glib::RefPtr<Gtk::RadioAction> refChoiceOne, m_refChoiceTwo;
+
+	void quitWindow();
+	void openSaved();
 };
